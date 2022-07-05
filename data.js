880205ad538cbf63b243515425d5fe580ca2c27d
daniel <daniel.bnh13@gmail.com>
2022-07-05 15:27:07 -0300

    nsei

--- data.js
+++ data.js
@@ -5,16 +5,13 @@ import { MongoClient,ObjectId } from "mongodb";
 const mongoClient = new MongoClient(process.env.mongo_conect);
 
 
-
-try{
-    await  mongoClient.connect();
-   
-     db = mongoClient.db(process.env.mongo_data);
-    console.log('Banco de dados conectado')
-    
-    
-}catch(e){
+try {
+    await mongoClient.connect();
+    db = mongoClient.db(process.env.MONGO_DB);
+    console.log('Banco de dados conectado com sucesso...');
+  } catch (error) {
     console.error('Aconteceu um problema ao conectar o banco de dados...');
-}
+  }
+
 
 export default db;
